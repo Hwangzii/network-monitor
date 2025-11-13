b1877bd6184ba2451091f16c0095cc0cce1bc7b1
khonghao0109 <khonghao0109@gmail.com>
2025-11-13 23:05:34 +0700

    Cập nhật kiến trúc

---
+++ MonitorApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using MonitorApp.ViewModels.Layouts;
+using MonitorApp.Views.Pages;
+using MonitorApp.Views.Pages.TrafficMonitor;
+
+
+
+namespace MonitorApp.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private object? _currentPage;
+
+        public HeaderViewModel HeaderVM { get; }
+
+        public object? CurrentPage
+        {
+            get => _currentPage;
+            set
+            {
+                _currentPage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HeaderVM = new HeaderViewModel();
+            HeaderVM.PageChanged += OnPageChanged;
+
+            // Trang mặc định
+            CurrentPage = new TM_Main();
+        }
+
+        private void OnPageChanged(string page)
+        {
+            switch (page)
+            {
+                case "Traffic":
+                    CurrentPage = new TM_Main();
+                    break;
+                case "GlassWire":
+                    CurrentPage = new GrassWireProtect();
+                    break;
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
